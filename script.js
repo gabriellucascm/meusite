4fa137180dc891a8c385d0004bd8ceb95226ae09
gabriellucascm <63599233+gabriellucascm@users.noreply.github.com>
2020-08-23 23:42:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("urss").addEventListener('click',function(){
+    alert("иди в гулаг прямо сейчас!");
+    window.open('http://www.youtube.com/watch?v=MJdz3i44dIc&t=0m5s');
+});
+document.getElementById("incel").addEventListener("click",function(){
+    alert("n tenho insta, botei só pra ficar bonitin :(");
+})
+
